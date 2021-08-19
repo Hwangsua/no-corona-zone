06de376edaf40800afb02d051c15c97715549dbc
LeeJongWan <lee2003121@naver.com>
2021-08-19 23:52:08 +0900

    0819 - skApi 1차

---
+++ src/main/java/com/megait/nocoronazone/repository/SafetyRepository.java
@@ -0,0 +1,7 @@
+package com.megait.nocoronazone.repository;
+import com.megait.nocoronazone.domain.Safety_Index;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SafetyRepository extends JpaRepository<Safety_Index, Long> {
+}
+
---
+++ src/main/java/com/megait/nocoronazone/repository/Seoul_SafetyRepository.java
@@ -0,0 +1,7 @@
+package com.megait.nocoronazone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.megait.nocoronazone.domain.Safety_Index_In_Seoul;
+
+public interface Seoul_SafetyRepository extends JpaRepository<Safety_Index_In_Seoul, Long> {
+}
